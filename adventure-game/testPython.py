a070c7834694c99889c97deb24ba03ebbaec010c
Gary Darer <garydarer@gmail.com>
2017-11-22 16:55:42 -0800

    removed file

--- adventure-game/testPython.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open ('pythonData.txt', 'r') as in_file:
-    rawStringContents = in_file.read()
-    jsonContents = json.loads(rawStringContents)
-
-    state = input()
-
-    print( jsonContents[state] )
