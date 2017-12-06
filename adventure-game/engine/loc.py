9a13721af1a752ab073c491b23f03cdc55e9c523
Nicolas Kent <ankiNicolas@github.com>
2017-12-05 23:16:59 -0800

    factored instance code out into a seperate file in the engine

--- adventure-game/engine/loc.py
+++ adventure-game/engine/loc.py
@@ -18,37 +18,37 @@ def translate( input_string, local_data=None ):
         # if result is not a string, assume its a list of strings and join them into a newline delimited string
         if not isinstance( result, str ):
             result = '\n'.join(result)
-
-        # Um, regular expressions are a whole mess of bullshit, but this means:
-        # "find all things inside {} and give me a list of them"
-        substitutions = re.findall('\{([^}]+)\}', result)
-        if substitutions:
-            for matchKey in substitutions:
-                # split the contained string within the braces into an array of keys
-                replaceKeyComponents = matchKey.split('.')
-                node = None
-                # check if the first key is an attribute of either the staticData or dynamicData classes
-                firstKey = replaceKeyComponents[0]
-                if local_data and firstKey in local_data:
-                    node = local_data[firstKey]
-                elif hasattr(dictionary.dynamic, firstKey):
-                    node = getattr(dictionary.dynamic, firstKey)
-                elif hasattr(dictionary.static, firstKey):
-                    node = getattr(dictionary.static, firstKey)
-
-                if node:
-                    # remove the first item from the list
-                    replaceKeyComponents.pop(0)
-                    # walk the json object using the keys
-                    for key in replaceKeyComponents:
-                        if key in node:
-                            node = node[key]
-                        else:
-                            node = None
-                            break
-                if node:
-                    # replace the string
-                    result = result.replace( "{" + matchKey + "}", str(node) )
-        return result
     else:
-        return input_string
+        result = input_string
+
+    # Um, regular expressions are a whole mess of bullshit, but this means:
+    # "find all things inside {} and give me a list of them"
+    substitutions = re.findall('\{([^}]+)\}', result)
+    if substitutions:
+        for matchKey in substitutions:
+            # split the contained string within the braces into an array of keys
+            replaceKeyComponents = matchKey.split('.')
+            node = None
+            # check if the first key is an attribute of either the staticData or dynamicData classes
+            firstKey = replaceKeyComponents[0]
+            if local_data and firstKey in local_data:
+                node = local_data[firstKey]
+            elif hasattr(dictionary.dynamic, firstKey):
+                node = getattr(dictionary.dynamic, firstKey)
+            elif hasattr(dictionary.static, firstKey):
+                node = getattr(dictionary.static, firstKey)
+
+            if node:
+                # remove the first item from the list
+                replaceKeyComponents.pop(0)
+                # walk the json object using the keys
+                for key in replaceKeyComponents:
+                    if key in node:
+                        node = node[key]
+                    else:
+                        node = None
+                        break
+            if node:
+                # replace the string
+                result = result.replace( "{" + matchKey + "}", str(node) )
+    return result
