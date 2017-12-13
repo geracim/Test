a8c31f733fc23bc44c56e2f622ce54e3f16e438a
Gary Darer <garydarer@gmail.com>
2017-12-12 16:19:00 -0800

    code that doesn't work
    stupid shit

---
+++ tkinter/tkinter_img_support/tkinter_gif_concept.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+THIS SHIT DON'T FUCKING WORK!
+"""
+
+from tkinter import *
+import time
+import os
+root = Tk()
+
+frames = [PhotoImage(file='mygif.gif',format = 'gif -index %i' %(i)) for i in range(100)]
+
+def update(ind):
+    frame = frames[ind]
+    ind += 1
+    label.configure(image=frame)
+    root.after(100, update, ind)
+label = Label(root)
+label.pack()
+root.after(0, update, 0)
+root.mainloop()
