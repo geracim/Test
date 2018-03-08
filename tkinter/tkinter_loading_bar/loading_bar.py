6cd2ca3e47c4f0a4c9e722ba744bfb583f21509a
Gary Darer <garydarer@gmail.com>
2018-03-07 17:25:00 -0800

    dfds
    dfd

--- tkinter/tkinter_loading_bar/loading_bar.py
+++ tkinter/tkinter_loading_bar/loading_bar.py
@@ -8,10 +8,13 @@ class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        # When you click start, the loading start(self) function runs
         self.button = ttk.Button(text="Load", command=self.start)
         self.button.pack()
+        # When you click reset, the reset(self) function runs
         self.button = ttk.Button(text="Reset", command=self.reset)
         self.button.pack()
+        # This draws the progress bar & determines the size & orientation
         self.progress = ttk.Progressbar(self, orient="horizontal",
                                         length=400, mode="determinate")
         self.progress.pack()
@@ -21,22 +24,22 @@ class SampleApp(tk.Tk):
 
     def start(self):
         self.progress["value"] = 0
-        self.maxbytes = 50000
-        self.progress["maximum"] = 50000
+        self.maxbytes = 100
+        self.progress["maximum"] = 100
         self.read_bytes()
 
     def reset(self):
         self.progress["value"] = 0
-        self.maxbytes = 50000
+        self.maxbytes = 100
         self.bytes = 0
 
     def read_bytes(self):
         '''simulate reading 500 bytes; update progress bar'''
-        self.bytes += 500
+        self.bytes += 5
         self.progress["value"] = self.bytes
         if self.bytes < self.maxbytes:
             # read more bytes after 100 ms
-            self.after(100, self.read_bytes)
+            self.after(8, self.read_bytes)
 
 app = SampleApp()
 app.mainloop()
