f9dededa9238c524c7a1e2c03af796342dd94953
Gary Darer <garydarer@gmail.com>
2018-03-22 13:34:31 -0700

    l

--- simulator/city_epochs.py
+++ simulator/city_epochs.py
@@ -10,12 +10,14 @@ class counters:
 	earthquake_counter = 0
 	fire_counter = 0
 	flood_counter = 0
+	plague_counter = 0
 	current_pop = 200
 
 class bools:
     earthquake_bool = False
     fire_bool = False
     flood_bool = False
+    plague_bool = False
 
 class constants:
 	earthquake_constant = 2
@@ -24,6 +26,8 @@ class constants:
 	fire_dmg = 0
 	flood_constant = 6
 	flood_dmg = 0
+	plague_constant = .01
+	plague_dmg = 0
 
 class rolls:
 	epoch_roll: 0
@@ -31,6 +35,7 @@ class rolls:
 	earthquake_roll: 0
 	fire_roll: 0
 	flood_roll: 0
+	plague_roll: 0
 
 def clear():
     if os.name == 'nt':
@@ -65,10 +70,22 @@ def flood():
 	else:
 		bools.flood_bool = False
 
+def plague():
+	rolls.plague_roll = random.randint(1,100)
+
+	if rolls.plague_roll > constants.plague_constant:
+		bools.plague_bool = True
+	else:
+		bools.plague_bool = False
+
 def new_pop():
 	rolls.pop_gain = random.randint(10,80)
 
-def dmg_roll()
+def dmg_roll():
+	constants.earthquake_dmg = random.randint(10, 80)
+	constants.fire_dmg = random.randint(1, 100)
+	constants.flood_dmg = random.randint(15, 50)
+	constants.plague_dmg = random.randint(1, counters.current_pop)
 	
 
 def run():
@@ -97,35 +114,45 @@ def run():
 #	print("earthquake roll: " + str(rolls.earthquake_roll) + "/" + str(constants.earthquake_constant))
 #	print("fire roll: " + str(rolls.fire_roll) + "/" + str(constants.fire_constant))
 #	print("flood roll: " + str(rolls.flood_roll) + "/" + str(constants.flood_constant))
+#	print("plague roll: " + str(rolls.plague_roll) + "/" + str(constants.plague_constant))
 #///// DEBUG ////////////
 	print("=======================================\n")
 # ================================================
 	if bools.earthquake_bool == True:
-		print("Earthquake: Yes")
-		print("-{} casualties\n".format(constants.earthquake_dmg))
+#		print("Earthquake: Yes")
+		print("Earthquake: {} casualties\n".format(constants.earthquake_dmg))
 		counters.current_pop -= 20
 		counters.earthquake_counter += 1
 		bools.earthquake_bool = False
 	else:
-		print("Earthquake: No \n")
+		print("Earthquake: No casualties.\n")
 # ================================================
 	if bools.fire_bool == True:
-		print("Fire: Yes")
-		print("-{} casualties\n".format(constants.fire_dmg))
+#		print("Fire: Yes")
+		print("Fire: {} casualties\n".format(constants.fire_dmg))
 		counters.current_pop -= 15
 		counters.fire_counter += 1
 		bools.fire_bool = False
 	else:
-		print("Fire: No\n")
+		print("Fire: No casualties.\n")
 # ================================================
 	if bools.flood_bool == True:
-		print("Flood: Yes")
-		print("-{} casualties\n".format(constants.flood_dmg))
+#		print("Flood: Yes")
+		print("Flood: {} casualties\n".format(constants.flood_dmg))
 		counters.current_pop -= 30
 		counters.flood_counter += 1
 		bools.flood_bool = False
 	else:
-		print("Flood: No\n")
+		print("Flood: No casualties.\n")
+# ================================================
+	if bools.plague_bool == True:
+#		print("Plague: Yes")
+		print("Plague: {} casualties\n".format(constants.plague_dmg))
+		counters.current_pop -= 30
+		counters.plague_counter += 1
+		bools.plague_bool = False
+	else:
+		print("Plague: No casualties.\n")
 # ================================================
 
 def play():
@@ -133,7 +160,6 @@ def play():
 
 	while counters.current_pop > 0 and counters.epoch_counter < rolls.epoch_roll:
 		run()
-
 		counters.epoch_counter += 1
 
 	if counters.epoch_counter > 1:
@@ -142,8 +168,6 @@ def play():
 		print("The City survived for {} century.".format(counters.epoch_counter))
 	else:
 		print("Epic Fail.")
-
-
 
 clear()
 play()
